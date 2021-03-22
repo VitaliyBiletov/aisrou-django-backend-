f31cbadb2c3271cde5a055b1c5c27bf9ef2c0ee6
VitaliyBiletov <vitaxa17@yandex.ru>
2021-03-22 08:55:17 +0300

    unite userform and pupilform

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -34,6 +34,7 @@ def users(request):
 
 
 def add_user(request):
+    heading = "Добавить"
     if request.method == "POST":
         user_form = UserForm(request.POST)
         profile_form = ProfileForm(request.POST)
@@ -57,6 +58,7 @@ def add_user(request):
             request,
             'admin_panel/user_form_add.html',
             {
+                'heading': heading,
                 'user_form': user_form,
                 'profile_form': profile_form,
             }
@@ -64,6 +66,7 @@ def add_user(request):
 
 
 def edit_user(request, id):
+    heading = "Изменить"
     if request.method == "POST":
         user = User.objects.get(pk=id)
         profile = Profile.objects.get(user_id=id)
@@ -82,6 +85,7 @@ def edit_user(request, id):
             request,
             'admin_panel/user_form_edit.html',
             {
+                'heading': heading,
                 'user_form': user_form,
                 'profile_form': profile_form,
                 'id': id
@@ -98,7 +102,6 @@ def delete_user(request, id):
 def pupils(request):
     list_pupils = Pupil.objects.all()
     page = request.GET.get('page', 1)
-
     paginator = Paginator(list_pupils, 5)
     try:
         pupils = paginator.page(page)
